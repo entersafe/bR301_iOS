587c90ee33af5d832346aa1925be8a9d0c9b568a
Feitian Technologies <hongbin@ftsafe.com>
2016-02-03 17:50:30 +0800

    Fix bug
    ix SCardGetStatusChange() may (not always, but frequently) return
    “SCARD_E_READER_UNAVAILABLE” problem

--- libs/source/src/winscard.m
+++ libs/source/src/winscard.m
@@ -96,7 +96,7 @@ extern pthread_mutex_t CommunicatonMutex;
 
 unsigned int isDukpt = 0;
 
-char Ft_iR301U_Version[3]={0x01,0x31,0x01};
+char Ft_iR301U_Version[3]={0x01,0x31,0x02};
 volatile int eStablishContextCount = 0;
 volatile int eShCardHandleCount = 0;
 
